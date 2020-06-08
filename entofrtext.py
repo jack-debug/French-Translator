f5cb13a5978bd3948e20df1a15c8cf0d0adf391b
jack-debug <55571956+jack-debug@users.noreply.github.com>
2020-06-09 09:52:38 +1000

    Update entofrtext.py

--- entofrtext.py
+++ entofrtext.py
@@ -1,4 +1,8 @@
 from translate import Translator
 
 setthing = 10
-if setthing == 10:
+print("Start typing")
+while setthing == 10:
+  totrans = input("")
+  fullytrans = translator.translate(totrans)
+  print(fullytrans)
