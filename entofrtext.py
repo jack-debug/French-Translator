46d9cfd2a9749b7711fa3bbd2e3736ae1cdb0a59
jack-debug <55571956+jack-debug@users.noreply.github.com>
2020-06-10 10:17:06 +1000

    Update entofrtext.py

--- entofrtext.py
+++ entofrtext.py
@@ -1,8 +1,7 @@
 from translate import Translator ## imports
 
-setthing = 10
 translator= Translator(to_lang="fr") ## initiating the translation
 print("Start typing")
-while setthing == 10: ## infinite loop
+while True: ## infinite loop
   totrans = input("") ## this takes in the text and stuff
   print(translator.translate(totrans)) ## it translates then prints without being in a variable
